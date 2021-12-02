76e784701c142eceda5d7823a324d00f4039b93d
NathanSilkensPXL <56999669+NathanSilkensPXL@users.noreply.github.com>
2021-12-02 16:07:34 +0100

    Edited deploy scripts

--- deployCore.ps1
+++ deployCore.ps1
@@ -1,6 +1,6 @@
-$STACK_NAME=Kaasje-htf2021-clueprocessing-core
-$MY_REGION=eu-central-1
-$MY_DEV_BUCKET=htf-deploymentbucket
+$STACK_NAME="Kaasje-htf2021-clueprocessing-core"
+$MY_REGION="eu-west-1"
+$MY_DEV_BUCKET="htf-deploymentbucket2"
 
 # Package new cloudformation package
 aws cloudformation package --template templates/core/template.yaml --s3-bucket $MY_DEV_BUCKET --output-template export-clue-processing-service.yaml --region $MY_REGION
